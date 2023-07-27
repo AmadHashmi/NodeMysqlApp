d28bd2f594e1a897c603ca910968feb82f7a4958
Amad Ul Hassan <amadhashmi179@gmail.com>
2023-07-27 23:16:04 +0200

    hiding credentials
    hiding credentials

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,13 @@
 "use strict";
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config(); // Load the .env file
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "node_mysql_crud",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 dbConn.connect(function (err) {
   if (err) throw err;
