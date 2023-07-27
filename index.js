a011b9154a4d1164e08f390fb2bd9a2bdbfac065
Amad Ul Hassan <amadhashmi179@gmail.com>
2023-07-27 23:05:30 +0200

    some modifications
    some modifications

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// // create express app
-// const app = express();
-// // Setup server port
-// const port = process.env.PORT || 3000;
-// // parse requests of content-type - application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: true }));
-// // parse requests of content-type - application/json
-// app.use(bodyParser.json());
-// // define a root route
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-// // Require user routes
-// const userRoutes = require("./src/routes/user.routes");
-// // using as middleware
-// app.use("/api/v1/users", userRoutes);
-// // listen for requests
-// app.listen(port, () => {
-//   console.log(`Server is listening on port ${port}`);
-// });
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const { check, validationResult } = require("express-validator");
